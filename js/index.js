5e72763841aaaa9f0fac6c2583f9c7092905f4aa
Mihail Makarov <m.makarov90@bk.ru>
2025-02-05 20:57:58 +0300

    add Elements in js

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const menuEl = document.querySelector('.menu');
+const menuTextEl = document.querySelector('.menu-text');
+const taskEl = document.querySelector('.task');
+const formEl = document.querySelector('.form');
+const buttonAddEl = document.querySelector('.btn-add');
+const btnDone = document.querySelector('#done');
+const btnDelete = document.querySelector('#delete');
